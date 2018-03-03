58c4649ff43e8179ad7327efc96131593dff6f1e
Wilson Cai <wcai@berkeley.edu>
2018-03-03 12:34:42 -0800

    complete blip var boot

--- R/avgDensityBootstrap.R
+++ R/avgDensityBootstrap.R
@@ -32,7 +32,6 @@ avgDensityBootstrap <- R6Class("avgDensityBootstrap",
 
         bootstrapOnestepFit <- avgDensityTMLE$new(x = d, epsilon_step = epsilon_step)
         # fit new density
-        # longDFOut_new <- self$pointTMLE$longDataOut$generate_df(x = d)
         longDFOut_new <- self$pointTMLE$longDataOut$generate_df_compress(x = d)
         HAL_boot <- fit_fixed_HAL(Y = longDFOut_new$Y,
           X = longDFOut_new[,'box'],
@@ -52,12 +51,6 @@ avgDensityBootstrap <- R6Class("avgDensityBootstrap",
         return(c(bootstrapOnestepFit$Psi))
       }
       library(foreach)
-
-      # library(doSNOW)
-      # library(tcltk)
-      # nw <- parallel:::detectCores()  # number of workers
-      # cl <- makeSOCKcluster(nw)
-      # registerDoSNOW(cl)
       all_bootstrap_estimates <- foreach(it2 = 1:(REPEAT_BOOTSTRAP), .combine = c,
                                          .inorder = FALSE,
                                          .packages = c('R6', 'SuperLearner'),
@@ -71,7 +64,6 @@ avgDensityBootstrap <- R6Class("avgDensityBootstrap",
       }
       # save(all_bootstrap_estimates, file = 'all_bootstrap_estimates.rda')
       # closeCluster(cl)
-      # stopCluster(cl)
 
       ALPHA <- 0.05
       # remove errors
@@ -95,7 +87,6 @@ avgDensityBootstrap <- R6Class("avgDensityBootstrap",
 
         bootstrapOnestepFit <- avgDensityTMLE$new(x = d, epsilon_step = epsilon_step)
         # fit new density
-        # longDFOut_new <- self$pointTMLE$longDataOut$generate_df(x = d)
         longDFOut_new <- self$pointTMLE$longDataOut$generate_df_compress(x = d)
         HAL_boot <- fit_fixed_HAL(Y = longDFOut_new$Y,
           X = longDFOut_new[,'box'],
@@ -127,11 +118,6 @@ avgDensityBootstrap <- R6Class("avgDensityBootstrap",
       }
 
       library(foreach)
-      # library(doSNOW)
-      # library(tcltk)
-      # nw <- parallel:::detectCores()  # number of workers
-      # cl <- makeSOCKcluster(nw)
-      # registerDoSNOW(cl)
       all_bootstrap_estimates <- foreach(it2 = 1:(REPEAT_BOOTSTRAP), .combine = c,
                                          .inorder = FALSE,
                                          .packages = c('R6', 'SuperLearner'),
@@ -156,91 +142,6 @@ avgDensityBootstrap <- R6Class("avgDensityBootstrap",
       normal_CI <- self$pointTMLE$CI
       self$CI_all <- list(normal_CI, boot1_CI)
     },
-    # bootstrap_2_compensate = function(REPEAT_BOOTSTRAP = 2e2){
-    #   SAMPLE_PER_BOOTSTRAP <- length(self$x)
-    #   betfun <- function(data,
-    #                      epsilon_step = self$epsilon_step,
-    #                      population_x = self$x,
-    #                      population_tmle = self$pointTMLE){
-    #     # indices is the random indexes for the bootstrap sample
-    #     indices <- sample(1:length(data), size = SAMPLE_PER_BOOTSTRAP, replace = TRUE) # user specify sample size
-    #     d = data[indices]
-
-    #     bootstrapOnestepFit <- avgDensityTMLE$new(x = d, epsilon_step = epsilon_step)
-    #     # fit new density
-    #     # longDFOut_new <- self$pointTMLE$longDataOut$generate_df(x = d)
-    #     longDFOut_new <- self$pointTMLE$longDataOut$generate_df_compress(x = d)
-    #     HAL_boot <- fit_fixed_HAL(Y = longDFOut_new$Y,
-    #       X = longDFOut_new[,'box'],
-    #       weights = longDFOut_new$Freq, # for df_compress only
-    #       hal9001_object = self$pointTMLE$HAL_tuned,
-    #       family = stats::binomial())
-    #     yhat_boot <- predict.fixed_HAL(HAL_boot, new_data = d)
-
-    #     yhat_boot[yhat_boot > 2*quantile(yhat_boot, probs = .75)] <- 0 # temporarily fix hal9001 extrapolation error
-    #     density_boot <- empiricalDensity$new(p_density = yhat_boot, x = d)
-    #     bootstrapOnestepFit$p_hat <- density_boot$normalize()
-    #     # target new fit
-    #     bootstrapOnestepFit$calc_Psi()
-    #     bootstrapOnestepFit$calc_EIC()
-    #     bootstrapOnestepFit$onestepTarget()
-
-    #     # get R2 term
-    #     # browser()
-    #     yhat_population <- predict.fixed_HAL(HAL_boot, new_data = population_x)
-    #     yhat_population[yhat_population > 2*quantile(yhat_population, probs = .75)] <- 0 # temporarily fix hal9001 extrapolation error
-    #     density_population <- empiricalDensity$new(p_density = yhat_population, x = population_x)
-    #     density_population$normalize()
-    #     ## integration
-    #     dummy_df <- data.frame(id = 1:length(population_x), x = population_x, p_pound = density_population$p_density, p_n = population_tmle$p_hat$p_density)
-    #     dummy_df <- dummy_df[order(dummy_df$x),]
-    #     dx <- c(0,diff(dummy_df$x))
-    #     R_2 <- -sum((dummy_df$p_pound - dummy_df$p_n)^2 * dx)
-
-    #     return(c(bootstrapOnestepFit$Psi - 2*R_2))
-    #   }
-
-    #   library(foreach)
-    #   all_bootstrap_estimates <- foreach(it2 = 1:(REPEAT_BOOTSTRAP), .combine = c,
-    #                                      .inorder = FALSE,
-    #                                      .packages = c('R6', 'SuperLearner'),
-    #                                      # .errorhandling = 'remove',
-    #                                      .errorhandling = 'pass',
-    #                                      .export = c('self'),
-    #                                      .verbose = F) %do% {
-    #                                      # .verbose = T) %dopar% {
-    #     if(it2 %% 10 == 0) print(it2)
-    #     betfun(self$x,
-    #            epsilon_step = self$epsilon_step,
-    #            population_x = self$x,
-    #            population_tmle = self$pointTMLE)
-    #   }
-    #   ALPHA <- 0.05
-    #   # remove errors
-    #   if( !all(sapply(all_bootstrap_estimates, class) == 'numeric') ) message(paste('Error happens.', sum(sapply(all_bootstrap_estimates, class) == 'numeric'), 'bootstraps are correct'))
-    #   all_bootstrap_estimates <- as.numeric(all_bootstrap_estimates[sapply(all_bootstrap_estimates, class) == 'numeric'])
-    #   self$bootstrap_estimates <- all_bootstrap_estimates
-
-    #   boot1_CI <- quantile(all_bootstrap_estimates, probs = c(ALPHA/2, 1 - ALPHA/2))
-    #   normal_CI <- self$pointTMLE$CI
-    #   self$CI_all <- list(normal_CI, boot1_CI)
-    # },
-    # wider_boot_CI = function(){
-    #   bootCI <- self$CI_all[[2]]
-    #   delta <- mean(bootCI) - self$Psi
-    #   bootCI[2] <- bootCI[2] + abs(delta)
-    #   bootCI[1] <- bootCI[1] - abs(delta)
-    #   new_CI <- bootCI
-    #   return(list(self$CI_all[[1]], self$CI_all[[2]], new_CI))
-    # },
-    # center_boot_CI = function(){
-    #   new_CI <- self$CI_all[[2]] - mean(self$CI_all[[2]]) + self$Psi
-    #   return(list(self$CI_all[[1]], self$CI_all[[2]], new_CI))
-    # },
-    # compensate_boot_CI = function(){
-    #   new_CI <- self$CI_all[[2]] - 2*(mean(self$CI_all[[2]]) - self$Psi)
-    #   return(list(self$CI_all[[1]], self$CI_all[[2]], new_CI))
-    # },
     penalized_boot_CI = function(){
       bootCI <- self$CI_all[[2]]
       delta <- mean(bootCI) - self$Psi
--- R/blipVarianceBootstrap.R
+++ R/blipVarianceBootstrap.R
@@ -5,14 +5,14 @@ blipVarianceBootstrap <- R6Class("blipVarianceBootstrap",
     data = NULL,
     pointTMLE = NULL,
     Psi = NULL,
-    
+
     CI_all = NULL,
     bootstrap_estimates = NULL,
     initialize = function(data, verbose = NULL) {
       self$data <- data
       if(class(data$W) != 'data.frame') message('W not data.frame')
       if (!is.null(verbose)) self$verbose <- verbose
-      self$pointTMLE <- blipVarianceTMLE_gentmle$new(data = df)
+      self$pointTMLE <- blipVarianceTMLE_gentmle$new(data = data)
       self$pointTMLE$initial_fit()
       self$pointTMLE$target()
 
@@ -27,7 +27,7 @@ blipVarianceBootstrap <- R6Class("blipVarianceBootstrap",
         d <- list(Y = data$Y[indices],
                   A = data$A[indices],
                   W = data.frame(data$W[indices,]))
-        
+
         bootstrapTmleFit <- blipVarianceTMLE_gentmle$new(data = d)
         # fit new Q, g
         library(hal9001)
@@ -54,7 +54,7 @@ blipVarianceBootstrap <- R6Class("blipVarianceBootstrap",
         # bootstrapTmleFit$Q_fit <- Q_HAL_boot
         # bootstrapTmleFit$g_fit <- g_HAL_boot
         bootstrapTmleFit$target()
-        
+
         return(c(bootstrapTmleFit$Psi))
       }
       library(foreach)
@@ -69,6 +69,81 @@ blipVarianceBootstrap <- R6Class("blipVarianceBootstrap",
                                            # .verbose = T) %dopar% {
                                            if(it2 %% 10 == 0) print(it2)
                                            betfun(self$data)
+                                         }
+      # save(all_bootstrap_estimates, file = 'all_bootstrap_estimates.rda')
+      ALPHA <- 0.05
+      # remove errors
+      if( !all(sapply(all_bootstrap_estimates, class) == 'numeric') ) message(paste('Error happens.', sum(sapply(all_bootstrap_estimates, class) == 'numeric'), 'bootstraps are correct'))
+      all_bootstrap_estimates <- as.numeric(all_bootstrap_estimates[sapply(all_bootstrap_estimates, class) == 'numeric'])
+      self$bootstrap_estimates <- all_bootstrap_estimates
+
+      boot1_CI <- quantile(all_bootstrap_estimates, probs = c(ALPHA/2, 1 - ALPHA/2))
+      normal_CI <- self$pointTMLE$CI
+      self$CI_all <- list(normal_CI, boot1_CI)
+    },
+    bootstrap_exact = function(REPEAT_BOOTSTRAP = 2e2){
+      SAMPLE_PER_BOOTSTRAP <- length(self$data$A)
+      betfun <- function(data, population_tmle){
+        # browser()
+        # indices is the random indexes for the bootstrap sample
+        indices <- sample(1:length(self$data$A), size = SAMPLE_PER_BOOTSTRAP, replace = TRUE) # user specify sample size
+        d <- list(Y = data$Y[indices],
+                  A = data$A[indices],
+                  W = data.frame(data$W[indices,]))
+
+        bootstrapTmleFit <- blipVarianceTMLE_gentmle$new(data = d)
+        # fit new Q, g
+        library(hal9001)
+        # Q fit
+        Q_HAL_boot <- fit_fixed_HAL(Y = d$Y,
+                                    X = data.frame(d$A, d$W),
+                                    hal9001_object = self$pointTMLE$Q_fit,
+                                    family = stats::binomial())
+        Q_AW_boot <- predict.fixed_HAL(Q_HAL_boot, new_data = data.frame(d$A, d$W))
+        Q_1W_boot <- predict.fixed_HAL(Q_HAL_boot, new_data = data.frame(1, d$W))
+        Q_0W_boot <- predict.fixed_HAL(Q_HAL_boot, new_data = data.frame(0, d$W))
+        # g fit
+        g_HAL_boot <- fit_fixed_HAL(Y = d$A,
+                                    X = d$W,
+                                    hal9001_object = self$pointTMLE$g_fit,
+                                    family = stats::binomial())
+        g_1W_boot <- predict.fixed_HAL(g_HAL_boot, new_data = data.frame(d$W))
+        # plug into tmle
+        bootstrapTmleFit$g_1W <- g_1W_boot
+        bootstrapTmleFit$g_AW <- g_1W_boot
+        bootstrapTmleFit$Q_AW <- Q_AW_boot
+        bootstrapTmleFit$Q_1W <- Q_1W_boot
+        bootstrapTmleFit$Q_0W <- Q_0W_boot
+        bootstrapTmleFit$Q_fit <- Q_HAL_boot
+        bootstrapTmleFit$g_fit <- g_HAL_boot
+        bootstrapTmleFit$target()
+
+        # predict Q#, g# on population data
+        g_pound_1 <- predict.fixed_HAL(bootstrapTmleFit$g_fit, new_data = data.frame(data$W))
+        Q_pound_1 <- predict.fixed_HAL(bootstrapTmleFit$Q_fit, new_data = data.frame(1, data$W))
+        Q_pound_0 <- predict.fixed_HAL(bootstrapTmleFit$Q_fit, new_data = data.frame(0, data$W))
+        B_pound <- Q_pound_1 - Q_pound_0
+        # get R2 term
+        term1 <- (population_tmle$Psi - bootstrapTmleFit$Psi)^2
+        # population_tmle$gentmle_object$tmledata$Q1k
+        term2 <- mean((population_tmle$Q_1W - population_tmle$Q_0W - B_pound)^2)
+        cross_prod <- (population_tmle$g_1W - g_pound_1)/g_pound_1*(population_tmle$Q_1W - Q_pound_1) - 
+          (1-population_tmle$g_1W - (1-g_pound_1))/(1-g_pound_1)*(population_tmle$Q_0W - Q_pound_0)
+        term3 <- mean(2*(B_pound - bootstrapTmleFit$Psi) * cross_prod)
+        # compute R2
+        R2 <- term1 - term2 + term3
+        return(bootstrapTmleFit$Psi - R2)
+      }
+      library(foreach)
+      all_bootstrap_estimates <- foreach(it2 = 1:(REPEAT_BOOTSTRAP),
+                                         .combine = c,
+                                         .inorder = FALSE,
+                                         .packages = c('R6', 'hal9001', 'fixedHAL'),
+                                         .errorhandling = 'pass',
+                                         .export = c('self'),
+                                         .verbose = F) %do% {
+                                           if(it2 %% 10 == 0) print(it2)
+                                           betfun(data = self$data, population_tmle = self$pointTMLE)
                                          }
       # save(all_bootstrap_estimates, file = 'all_bootstrap_estimates.rda')
       ALPHA <- 0.05
