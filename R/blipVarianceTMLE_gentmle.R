ed9fd8579732e1d3ec6e8bba28f1bf838ba595b7
Wilson Cai <wcai@berkeley.edu>
2018-03-15 14:00:25 -0700

    complete contY tmle

--- R/blipVarianceTMLE_gentmle.R
+++ R/blipVarianceTMLE_gentmle.R
@@ -9,11 +9,12 @@ blipVarianceTMLE_gentmle <- R6Class("blipVarianceTMLE_gentmle",
     Q_1W = NULL,
     Q_0W = NULL,
     g_fit = NULL,
-    g_AW = NULL,
+    # g_AW = NULL,
     g_1W = NULL,
 
     Psi = NULL,
     gentmle_object = NULL,
+    se_Psi = NULL,
     CI = NULL,
     verbose = FALSE,
     initialize = function(data, epsilon_step = NULL, verbose = NULL) {
@@ -40,14 +41,14 @@ blipVarianceTMLE_gentmle <- R6Class("blipVarianceTMLE_gentmle",
                                      fit_type = 'glmnet',
                                      family = 'binomial',
                                      yolo = FALSE)
-      self$g_AW <- plogis(hal9001:::predict.hal9001(self$g_fit, new_data = data.frame(self$data$W)))
+      # self$g_AW <- plogis(hal9001:::predict.hal9001(self$g_fit, new_data = data.frame(self$data$W)))
       self$g_1W <- plogis(hal9001:::predict.hal9001(self$g_fit, new_data = data.frame(self$data$W)))
     },
     target = function() {
       library(gentmle2)
       initdata <- data.frame(A = self$data$A,
                              Y = self$data$Y,
-                             gk = self$g_AW,
+                             gk = self$g_1W,
                              Qk = self$Q_AW,
                              Q1k = self$Q_1W,
                              Q0k = self$Q_0W)
@@ -58,8 +59,8 @@ blipVarianceTMLE_gentmle <- R6Class("blipVarianceTMLE_gentmle",
                                      submodel = submodel_logit)
       self$Psi <- self$gentmle_object$tmleests
 
-      se_Psi <- sd(self$gentmle_object$Dstar)/sqrt(length(self$data$A))
-      self$CI <- self$Psi + c(-1.96, 1.96) * se_Psi
+      self$se_Psi <- sd(self$gentmle_object$Dstar)/sqrt(length(self$data$A))
+      self$CI <- self$Psi + c(-1.96, 1.96) * self$se_Psi
     }
 ))
 
@@ -67,44 +68,76 @@ blipVarianceTMLE_gentmle <- R6Class("blipVarianceTMLE_gentmle",
 blipVarianceTMLE_gentmle_contY <- R6Class("blipVarianceTMLE_gentmle_contY",
   inherit = blipVarianceTMLE_gentmle,
   public = list(
-    # initial_fit = function(){
-    #   library(hal9001)
-    #   # Q fit
-    #   self$Q_fit <- hal9001::fit_hal(X = data.frame(self$data$A, self$data$W),
-    #                                  Y = self$data$Y,
-    #                                  n_folds = 3,
-    #                                  fit_type = 'glmnet',
-    #                                  family = 'binomial',
-    #                                  yolo = FALSE)
-    #   self$Q_AW <- plogis(hal9001:::predict.hal9001(self$Q_fit, new_data = data.frame(self$data$A, self$data$W)))
-    #   self$Q_1W <- plogis(hal9001:::predict.hal9001(self$Q_fit, new_data = data.frame(1, self$data$W)))
-    #   self$Q_0W <- plogis(hal9001:::predict.hal9001(self$Q_fit, new_data = data.frame(0, self$data$W)))
-    #   # g fit
-    #   self$g_fit <- hal9001::fit_hal(X = self$data$W,
-    #                                  Y = self$data$A,
-    #                                  n_folds = 3,
-    #                                  fit_type = 'glmnet',
-    #                                  family = 'binomial',
-    #                                  yolo = FALSE)
-    #   self$g_AW <- plogis(hal9001:::predict.hal9001(self$g_fit, new_data = data.frame(self$data$W)))
-    #   self$g_1W <- plogis(hal9001:::predict.hal9001(self$g_fit, new_data = data.frame(self$data$W)))
-    # },
-    # target = function() {
-    #   library(gentmle2)
-    #   initdata <- data.frame(A = self$data$A,
-    #                          Y = self$data$Y,
-    #                          gk = self$g_AW,
-    #                          Qk = self$Q_AW,
-    #                          Q1k = self$Q_1W,
-    #                          Q0k = self$Q_0W)
-    #   self$gentmle_object <- gentmle(initdata = initdata,
-    #                                  params = list(param_sigmaATE),
-    #                                  approach = "full",
-    #                                  # max_iter = 1e5,
-    #                                  submodel = submodel_logit)
-    #   self$Psi <- self$gentmle_object$tmleests
-    # 
-    #   se_Psi <- sd(self$gentmle_object$Dstar)/sqrt(length(self$data$A))
-    #   self$CI <- self$Psi + c(-1.96, 1.96) * se_Psi
-    # }
+    # continuous Y
+    min_Y = NULL,
+    max_Y = NULL,
+    range_Y = NULL,
+    Y_rescale = NULL,
+
+    min_Q = NULL,
+    max_Q = NULL,
+    range_Q = NULL,
+    Q_AW_rescale = NULL,
+    Q_1W_rescale = NULL,
+    Q_0W_rescale = NULL,
+    scaleY = function(){
+      # scale Y to (0,1)
+      self$min_Y <- min(self$data$Y)
+      self$max_Y <- max(self$data$Y)
+      self$range_Y <- self$max_Y - self$min_Y
+      self$Y_rescale <- (self$data$Y - self$min_Y)/self$range_Y
+    },
+    scaleBack_afterTMLE = function(){
+      self$Psi <- self$Psi * self$range_Y^2 # variance is rescaled
+      self$se_Psi <- self$se_Psi * self$range_Y^2 # EIC is scaled by the same amount
+      self$CI <- self$Psi + c(-1.96, 1.96) * self$se_Psi
+    },
+    initial_fit = function(){
+      # message('continuous Y')
+      library(hal9001)
+      # Q fit
+      self$Q_fit <- hal9001::fit_hal(X = data.frame(self$data$A, self$data$W),
+                                     Y = self$data$Y,
+                                     n_folds = 3,
+                                     fit_type = 'glmnet',
+                                     family = 'gaussian',
+                                     yolo = FALSE)
+      self$Q_AW <- hal9001:::predict.hal9001(self$Q_fit, new_data = data.frame(self$data$A, self$data$W))
+      self$Q_1W <- hal9001:::predict.hal9001(self$Q_fit, new_data = data.frame(1, self$data$W))
+      self$Q_0W <- hal9001:::predict.hal9001(self$Q_fit, new_data = data.frame(0, self$data$W))
+      # g fit
+      self$g_fit <- hal9001::fit_hal(X = self$data$W,
+                                     Y = self$data$A,
+                                     n_folds = 3,
+                                     fit_type = 'glmnet',
+                                     family = 'binomial',
+                                     yolo = FALSE)
+      # self$g_AW <- plogis(hal9001:::predict.hal9001(self$g_fit, new_data = data.frame(self$data$W)))
+      self$g_1W <- plogis(hal9001:::predict.hal9001(self$g_fit, new_data = data.frame(self$data$W)))
+      # scale Q to (0,1)
+      self$min_Q <- min(self$Q_1W, self$Q_0W)
+      self$max_Q <- max(self$Q_1W, self$Q_0W)
+      self$range_Q <- self$max_Q - self$min_Q
+      self$Q_AW_rescale <- (self$Q_AW - self$min_Q) / self$range_Q
+      self$Q_1W_rescale <- (self$Q_1W - self$min_Q) / self$range_Q
+      self$Q_0W_rescale <- (self$Q_0W - self$min_Q) / self$range_Q
+    },
+    target = function() {
+      # message('continuous Y')
+      library(gentmle2)
+      initdata <- data.frame(A = self$data$A,
+                             Y = self$Y_rescale,
+                             gk = self$g_1W,
+                             Qk = self$Q_AW_rescale,
+                             Q1k = self$Q_1W_rescale,
+                             Q0k = self$Q_0W_rescale)
+      self$gentmle_object <- gentmle(initdata = initdata,
+                                     params = list(param_sigmaATE),
+                                     approach = "full",
+                                     # max_iter = 1e5,
+                                     submodel = submodel_logit)
+      self$Psi <- self$gentmle_object$tmleests
+      self$se_Psi <- sd(self$gentmle_object$Dstar)/sqrt(length(self$data$A))
+      self$CI <- self$Psi + c(-1.96, 1.96) * self$se_Psi
+    }
 ))
