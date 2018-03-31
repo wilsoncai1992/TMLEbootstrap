7bd3833878a47a3db402cbcac90113866a9fd0d9
Wilson Cai <wcai@berkeley.edu>
2018-03-31 10:29:30 -0700

    make up the `regular boot` method for `blipVar-contY`

--- R/blipVarianceBootstrap.R
+++ R/blipVarianceBootstrap.R
@@ -48,7 +48,6 @@ blipVarianceBootstrap <- R6Class("blipVarianceBootstrap",
         g_1W_boot <- predict.fixed_HAL(g_HAL_boot, new_data = data.frame(d$W))
         # plug into tmle
         bootstrapTmleFit$g_1W <- g_1W_boot
-        bootstrapTmleFit$g_AW <- g_1W_boot
         bootstrapTmleFit$Q_AW <- Q_AW_boot
         bootstrapTmleFit$Q_1W <- Q_1W_boot
         bootstrapTmleFit$Q_0W <- Q_0W_boot
@@ -296,6 +295,69 @@ blipVarianceBootstrap_contY <- R6Class("blipVarianceBootstrap_contY",
                                          # .verbose = F) %dopar% {
                                            if(it2 %% 10 == 0) print(it2)
                                            betfun(data = self$data, population_tmle = self$pointTMLE)
+                                         }
+      # save(all_bootstrap_estimates, file = 'all_bootstrap_estimates.rda')
+      ALPHA <- 0.05
+      # remove errors
+      if( !all(sapply(all_bootstrap_estimates, class) == 'numeric') ) message(paste('Error happens.', sum(sapply(all_bootstrap_estimates, class) == 'numeric'), 'bootstraps are correct'))
+      all_bootstrap_estimates <- as.numeric(all_bootstrap_estimates[sapply(all_bootstrap_estimates, class) == 'numeric'])
+      self$bootstrap_estimates <- all_bootstrap_estimates
+
+      boot1_CI <- quantile(all_bootstrap_estimates, probs = c(ALPHA/2, 1 - ALPHA/2))
+      normal_CI <- self$pointTMLE$CI
+      self$CI_all <- list(normal_CI, boot1_CI)
+    },
+    bootstrap = function(REPEAT_BOOTSTRAP = 2e2){
+      # regular bootstrap; use continuous HAL bootstrap fit
+      SAMPLE_PER_BOOTSTRAP <- length(self$data$A)
+      betfun <- function(data){
+        # indices is the random indexes for the bootstrap sample
+        indices <- sample(1:length(self$data$A), size = SAMPLE_PER_BOOTSTRAP, replace = TRUE) # user specify sample size
+        d <- list(Y = data$Y[indices],
+                  A = data$A[indices],
+                  W = data.frame(data$W[indices,]))
+
+        bootstrapTmleFit <- blipVarianceTMLE_gentmle_contY$new(data = d)
+        bootstrapTmleFit$scaleY() # not rigorous; use population scale_Y
+        # fit new Q, g
+        library(hal9001)
+        # Q fit
+        Q_HAL_boot <- fit_fixed_HAL(Y = d$Y,
+                                    X = data.frame(d$A, d$W),
+                                    hal9001_object = self$pointTMLE$Q_fit,
+                                    family = stats::gaussian())
+        Q_AW_boot <- predict.fixed_HAL(Q_HAL_boot, new_data = data.frame(d$A, d$W))
+        Q_1W_boot <- predict.fixed_HAL(Q_HAL_boot, new_data = data.frame(1, d$W))
+        Q_0W_boot <- predict.fixed_HAL(Q_HAL_boot, new_data = data.frame(0, d$W))
+        # g fit
+        g_HAL_boot <- fit_fixed_HAL(Y = d$A,
+                                    X = d$W,
+                                    hal9001_object = self$pointTMLE$g_fit,
+                                    family = stats::binomial())
+        g_1W_boot <- predict.fixed_HAL(g_HAL_boot, new_data = data.frame(d$W))
+
+        # plug into tmle
+        bootstrapTmleFit$g_1W <- g_1W_boot
+        bootstrapTmleFit$Q_AW_rescale <- self$pointTMLE$scale_Q$scale01(newX = Q_AW_boot) # scale to (0,1) because continuous
+        bootstrapTmleFit$Q_1W_rescale <- self$pointTMLE$scale_Q$scale01(newX = Q_1W_boot)
+        bootstrapTmleFit$Q_0W_rescale <- self$pointTMLE$scale_Q$scale01(newX = Q_0W_boot)
+        bootstrapTmleFit$Q_fit <- Q_HAL_boot
+        bootstrapTmleFit$g_fit <- g_HAL_boot
+        bootstrapTmleFit$target()
+        bootstrapTmleFit$scaleBack_afterTMLE() # cont Y
+        return(bootstrapTmleFit$Psi)
+      }
+      library(foreach)
+      all_bootstrap_estimates <- foreach(it2 = 1:(REPEAT_BOOTSTRAP),
+                                         .combine = c,
+                                         .inorder = FALSE,
+                                         .packages = c('R6', 'hal9001', 'fixedHAL'),
+                                         .errorhandling = 'pass',
+                                         .export = c('self'),
+                                         .verbose = F) %do% {
+                                         # .verbose = F) %dopar% {
+                                           if(it2 %% 10 == 0) print(it2)
+                                           betfun(data = self$data)
                                          }
       # save(all_bootstrap_estimates, file = 'all_bootstrap_estimates.rda')
       ALPHA <- 0.05
