6a13f2185bba8b444fb787f7d4199856042d4606
Wilson Cai <wcai@berkeley.edu>
2018-04-11 00:26:02 -0700

    use list

--- R/avgDensity_LambdaGrid.R
+++ R/avgDensity_LambdaGrid.R
@@ -7,13 +7,13 @@ avgDensity_LambdaGrid <- R6Class("avgDensity_LambdaGrid",
     REPEAT_BOOTSTRAP = NULL,
     inflate_lambda = NULL,
 
-    dict_boot = dict::dict(), # dictionary of comprehensiveBootstrap
+    # dict_boot = dict::dict(), # dictionary of comprehensiveBootstrap
+    dict_boot = list(), # named list of comprehensiveBootstrap
     initialize = function(data,
                           bin_width,
                           epsilon_step,
                           REPEAT_BOOTSTRAP = 2e2,
                           inflate_lambda = 1) {
-      library(dict)
       self$data <- data
       self$bin_width <- bin_width
       self$epsilon_step <- epsilon_step
@@ -21,6 +21,7 @@ avgDensity_LambdaGrid <- R6Class("avgDensity_LambdaGrid",
       self$inflate_lambda <- inflate_lambda
     },
     add_lambda = function(lambda_grid = NULL) {
+      new_ls <- list()
       for (lambda in lambda_grid) {
         boot_here <- comprehensiveBootstrap$new(parameter = avgDensityBootstrap,
                                                 x = self$data$x,
@@ -28,10 +29,21 @@ avgDensity_LambdaGrid <- R6Class("avgDensity_LambdaGrid",
                                                 epsilon_step = self$epsilon_step)
         boot_here$bootstrap(REPEAT_BOOTSTRAP = self$REPEAT_BOOTSTRAP,
                             inflate_lambda = self$inflate_lambda)
+        boot_here$all_CI()
         boot_here$compute_width()
-        self$dict_boot[[lambda]] <- boot_here
+        new_ls <- c(new_ls, boot_here)
+        # self$dict_boot[[lambda]] <- boot_here
         message(paste(lambda, 'is added'))
       }
-    }
+      names(new_ls) <- lambda_grid
+      self$dict_boot <- c(self$dict_boot, new_ls)
+    },
+    get_lambda = function() {
+      as.numeric(names(self$dict_boot))
+    },
+    get_value = function() {
+      unname(self$dict_boot)
+    },
+    
   )
 )
